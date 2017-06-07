dff9dc7fcb5ff8599e9facf04c46b0daa5eddf49
Roman Simecek <roman@good2go.ch>
2017-06-07 10:18:13 +0200

    Create a railtie which loads the glue.

---
+++ lib/activerecord/oracle/queue/railtie.rb
@@ -0,0 +1,17 @@
+require "railties"
+require "activerecord/oracle/queue/glue"
+
+module Activerecord
+  module Oracle
+    module Queue
+      class Railtie < Rails::Railtie
+        initializer "activerecord.oracle.queue.insert_into_active_record" do |app|
+          ActiveRecord::Base.send(
+            :include,
+            Activerecord::Oracle::Queue::Glue
+          )
+        end
+      end
+    end
+  end
+end
